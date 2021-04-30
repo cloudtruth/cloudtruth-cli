f23fd2d8cea3a1c6ae6668f4cba44860af0d0b07
Rick Porter <rick.porter@cloudtruth.com>
2021-04-30 14:33:39 -0400

    Add basic test for secret parameters

--- tests/pytest/test_parameters.py
+++ tests/pytest/test_parameters.py
@@ -1,6 +1,8 @@
 from testcase import TestCase
 
+
 class TestParameters(TestCase):
+
     def test_parameter_basic(self):
         base_cmd = self.get_cli_base_cmd()
         cmd_env = self.get_cmd_env()
@@ -8,15 +10,16 @@ class TestParameters(TestCase):
         # add a new project
         proj_name = "test-param-basic"
         empty_msg = f"No parameters found in project {proj_name}"
-        result = self.run_cli(cmd_env,base_cmd + f" projects set {proj_name} -d 'test_parameters_basic_crud() test'")
+        result = self.run_cli(cmd_env,
+                              base_cmd + f" projects set {proj_name} -d 'test_parameters_basic() test'")
         self.assertEqual(result.return_value, 0)
-    
+
         # check that there are no parameters
         sub_cmd = base_cmd + f" --project {proj_name} parameters "
         result = self.run_cli(cmd_env, sub_cmd + "list")
         self.assertEqual(result.return_value, 0)
         self.assertTrue(result.out_contains_value(empty_msg))
-    
+
         # same result with the --values flag
         result = self.run_cli(cmd_env, sub_cmd + "list --values")
         self.assertEqual(result.return_value, 0)
@@ -32,9 +35,10 @@ class TestParameters(TestCase):
         key1 = "my_param"
         value1 = "cRaZy value"
         desc1 = "this is just a test description"
-        result = self.run_cli(cmd_env, sub_cmd + f"set {key1} --value \"{value1}\" --desc \"{desc1}\"")
+        result = self.run_cli(cmd_env,
+                              sub_cmd + f"set {key1} --value \"{value1}\" --desc \"{desc1}\"")
         self.assertEqual(result.return_value, 0)
-    
+
         result = self.run_cli(cmd_env, sub_cmd + f"ls -v")
         self.assertTrue(result.out_contains_both(key1, value1))
         self.assertTrue(result.out_contains_both(key1, desc1))
@@ -49,13 +53,84 @@ class TestParameters(TestCase):
         # delete the parameter
         result = self.run_cli(cmd_env, sub_cmd + f"delete {key1}")
         self.assertEqual(result.return_value, 0)
-    
+
         # make sure it is gone
         result = self.run_cli(cmd_env, sub_cmd + "list --values --secrets")
         self.assertEqual(result.return_value, 0)
         self.assertTrue(result.out_contains_value(empty_msg))
-    
+
         # delete the project
-        result = self.run_cli(cmd_env,base_cmd + f" projects delete {proj_name} --confirm")
+        result = self.run_cli(cmd_env, base_cmd + f" projects delete {proj_name} --confirm")
         self.assertEqual(result.return_value, 0)
 
+    def test_parameter_secret(self):
+        base_cmd = self.get_cli_base_cmd()
+        cmd_env = self.get_cmd_env()
+
+        # add a new project
+        proj_name = "test-param-secret"
+        empty_msg = f"No parameters found in project {proj_name}"
+        result = self.run_cli(cmd_env,
+                              base_cmd + f" projects set {proj_name} -d 'test_parameters_secret() test'")
+        self.assertEqual(result.return_value, 0)
+
+        # check that there are no parameters
+        sub_cmd = base_cmd + f" --project {proj_name} parameters "
+        result = self.run_cli(cmd_env, sub_cmd + "list")
+        self.assertEqual(result.return_value, 0)
+        self.assertTrue(result.out_contains_value(empty_msg))
+
+        # same result with the --values flag
+        result = self.run_cli(cmd_env, sub_cmd + "list --values")
+        self.assertEqual(result.return_value, 0)
+        self.assertTrue(result.out_contains_value(empty_msg))
+
+        # same result with the --values and --secrets flag
+        result = self.run_cli(cmd_env, sub_cmd + "list --values --secrets")
+        self.assertEqual(result.return_value, 0), "Initial empty parameters"
+        self.assertTrue(result.out_contains_value(empty_msg))
+
+        ########
+        # add first, secret parameter
+        key1 = "my_param"
+        value1 = "super-SENSITIVE-vAluE"
+        desc1 = "my secret value"
+        result = self.run_cli(cmd_env,
+                              sub_cmd + f"set {key1} --secret true --value \"{value1}\" --desc \"{desc1}\"")
+        self.assertEqual(result.return_value, 0)
+
+        result = self.run_cli(cmd_env, sub_cmd + f"ls -v")
+        self.assertFalse(result.out_contains_both(key1, value1))
+        self.assertTrue(result.out_contains_both(key1, desc1))
+        self.assertTrue(result.out_equals("""\
++----------+-------+---------+-----------------+
+| Name     | Value | Source  | Description     |
++----------+-------+---------+-----------------+
+| my_param | ***** | default | my secret value |
++----------+-------+---------+-----------------+
+"""))
+
+        # now, display with the secrets value
+        result = self.run_cli(cmd_env, sub_cmd + f"list --values --secrets")
+        self.assertTrue(result.out_contains_both(key1, value1))
+        self.assertTrue(result.out_contains_both(key1, desc1))
+        self.assertTrue(result.out_equals("""\
++----------+-----------------------+---------+-----------------+
+| Name     | Value                 | Source  | Description     |
++----------+-----------------------+---------+-----------------+
+| my_param | super-SENSITIVE-vAluE | default | my secret value |
++----------+-----------------------+---------+-----------------+
+"""))
+
+        # delete the parameter
+        result = self.run_cli(cmd_env, sub_cmd + f"delete {key1}")
+        self.assertEqual(result.return_value, 0)
+
+        # make sure it is gone
+        result = self.run_cli(cmd_env, sub_cmd + "list --values --secrets")
+        self.assertEqual(result.return_value, 0)
+        self.assertTrue(result.out_contains_value(empty_msg))
+
+        # delete the project
+        result = self.run_cli(cmd_env, base_cmd + f" projects delete {proj_name} --confirm")
+        self.assertEqual(result.return_value, 0)
