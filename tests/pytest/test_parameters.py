afc45ec714d8d638b780a8ea5aba4e78577a62e3
Rick Porter <rick.porter@cloudtruth.com>
2021-05-03 17:51:17 -0400

    Test parameter switch from/to secret

--- tests/pytest/test_parameters.py
+++ tests/pytest/test_parameters.py
@@ -444,3 +444,75 @@ FIRST_PARAM=posix_compliant_value
         # cleanup (no need to delete individual parameters)
         self.delete_project(cmd_env, proj_name)
 
+    def test_parameter_secret_switch(self):
+        base_cmd = self.get_cli_base_cmd()
+        cmd_env = self.get_cmd_env()
+
+        # add a new project
+        proj_name = "test-secret-switch"
+        empty_msg = self._empty_message(proj_name)
+        self.create_project(cmd_env, proj_name)
+
+        # check that there are no parameters
+        sub_cmd = base_cmd + f" --project {proj_name} parameters "
+        result = self.run_cli(cmd_env, sub_cmd + "list --values --secrets")
+        self.assertEqual(result.return_value, 0), "Initial empty parameters"
+        self.assertTrue(result.out_contains_value(empty_msg))
+
+        ########
+        # add first, non-secret parameter
+        key1 = "my_param"
+        value1 = "cRaZy value"
+        desc1 = "this is just a test description"
+        result = self.run_cli(cmd_env,
+                              sub_cmd + f"set {key1} --value '{value1}' --desc '{desc1}'")
+        self.assertEqual(result.return_value, 0)
+
+        result = self.run_cli(cmd_env, sub_cmd + f"ls -v")
+        self.assertEqual(result.out(), """\
++----------+-------------+---------+---------------------------------+
+| Name     | Value       | Source  | Description                     |
++----------+-------------+---------+---------------------------------+
+| my_param | cRaZy value | default | this is just a test description |
++----------+-------------+---------+---------------------------------+
+""")
+
+        # switch it to a secret
+        result = self.run_cli(cmd_env, sub_cmd + f"set {key1} --secret true")
+        self.assertEqual(result.return_value, 0)
+
+        # see that it has been changed to a secret (redacted in cli)
+        result = self.run_cli(cmd_env, sub_cmd + f"ls -v")
+        self.assertEqual(result.out(), """\
++----------+-------+---------+---------------------------------+
+| Name     | Value | Source  | Description                     |
++----------+-------+---------+---------------------------------+
+| my_param | ***** | default | this is just a test description |
++----------+-------+---------+---------------------------------+
+""")
+
+        # verify value has not changed
+        result = self.run_cli(cmd_env, sub_cmd + f"ls -v -s")
+        self.assertEqual(result.out(), """\
++----------+-------------+---------+---------------------------------+
+| Name     | Value       | Source  | Description                     |
++----------+-------------+---------+---------------------------------+
+| my_param | cRaZy value | default | this is just a test description |
++----------+-------------+---------+---------------------------------+
+""")
+
+        # switch back to a regular parameter
+        result = self.run_cli(cmd_env, sub_cmd + f"set {key1} --secret false")
+        self.assertEqual(result.return_value, 0)
+
+        # see that it is no longer redacted
+        result = self.run_cli(cmd_env, sub_cmd + f"ls -v")
+        self.assertEqual(result.out(), """\
++----------+-------------+---------+---------------------------------+
+| Name     | Value       | Source  | Description                     |
++----------+-------------+---------+---------------------------------+
+| my_param | cRaZy value | default | this is just a test description |
++----------+-------------+---------+---------------------------------+
+""")
+
+        self.delete_project(cmd_env, proj_name)
