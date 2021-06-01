bd7ca53d503df1a6e4860544b1cd08de6f57f5b5
Rick Porter <rick.porter@cloudtruth.com>
2021-06-01 11:56:52 -0400

    Add AES-GCM to wrap/unwrap capabilities

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,3 +1,4 @@
+use aes_gcm::Aes256Gcm;
 use base64::{self, DecodeError as Base64Error};
 use chacha20poly1305::aead::{AeadInPlace, NewAead};
 use chacha20poly1305::ChaCha20Poly1305;
@@ -16,7 +17,6 @@ const UNKNOWN_STR: &str = "unknown";
 
 const ENCODED_PART_COUNT: usize = 5;
 const KEY_LEN: usize = 32;
-//const TAG_LEN: usize = 16;
 const NONCE_LEN: usize = 12;
 
 #[derive(PartialEq, Eq, Hash, Debug)]
@@ -222,9 +222,62 @@ fn unwrap_chacha20_poly1305(jwt: &[u8], wrapper: &SecretWrapper) -> Result<Vec<u
     }
 }
 
+/// Wraps the plaintext using the AES-GCM algorithm with the `jwt` to generate the key, and returns
+/// an encoded string.
+fn wrap_aes_gcm(jwt: &[u8], plaintext: &[u8]) -> Result<String, Error> {
+    // derive the key from the JWT
+    let derived = generate_key(&jwt, None, None)?;
+    let key = aes_gcm::Key::from_slice(&derived);
+
+    // generate a new Nonce
+    let mut rand_bytes = [0u8; NONCE_LEN];
+    let mut rng = rand_core::OsRng;
+    rng.fill_bytes(&mut rand_bytes);
+    let nonce = aes_gcm::Nonce::from_slice(&rand_bytes);
+
+    let cipher = Aes256Gcm::new(key);
+    let mut in_out = vec![0; plaintext.len()];
+    in_out.copy_from_slice(&plaintext);
+    let result = cipher.encrypt_in_place_detached(&nonce, &[], &mut in_out);
+    match result {
+        Ok(tag) => {
+            let cipher_str = base64::encode(in_out);
+            let nonce_str = base64::encode(nonce);
+            let tag_str = base64::encode(tag);
+            let encoded = encode(
+                Algorithm::AesGcm,
+                nonce_str.as_str(),
+                cipher_str.as_str(),
+                tag_str.as_str(),
+            );
+            Ok(encoded)
+        }
+        Err(err) => Err(Error::Encrypt(err.to_string())),
+    }
+}
+
+/// Unwraps the ciphertext (inside the `SecretWrapper`) using the AES-GCM algorithm with the `jwt`
+/// to generate the key, and returns the plaintext on success.
+fn unwrap_aes_gcm(jwt: &[u8], wrapper: &SecretWrapper) -> Result<Vec<u8>, Error> {
+    let derived = generate_key(jwt, None, None)?;
+    let key = aes_gcm::Key::from_slice(&derived);
+    let cipher = Aes256Gcm::new(key);
+    let nonce_bytes = base64::decode(&wrapper.nonce)?;
+    let nonce = aes_gcm::Nonce::from_slice(&nonce_bytes);
+    let mut in_out = base64::decode(wrapper.cipher_text.as_str())?;
+    let tag_bytes = base64::decode(&wrapper.tag)?;
+    let tag = aes_gcm::Tag::from_slice(&tag_bytes);
+    let result = cipher.decrypt_in_place_detached(&nonce, &[], &mut in_out, tag);
+    match result {
+        Ok(_) => Ok(in_out),
+        Err(err) => Err(Error::Decrypt(err.to_string())),
+    }
+}
+
 /// Use the JWT to wrap the plaintext string in the specified algorithm
 pub fn wrap(algorithm: Algorithm, jwt: &[u8], plaintext: &[u8]) -> Result<String, Error> {
     match algorithm {
+        Algorithm::AesGcm => wrap_aes_gcm(jwt, plaintext),
         Algorithm::ChaCha20 => wrap_chacha20_poly1305(jwt, plaintext),
         _ => Err(Error::UnsupportedAlgorithm(format!("{}", algorithm))),
     }
@@ -234,6 +287,7 @@ pub fn wrap(algorithm: Algorithm, jwt: &[u8], plaintext: &[u8]) -> Result<String
 pub fn unwrap(jwt: &[u8], encoded: &str) -> Result<Vec<u8>, Error> {
     let wrapper = decode(encoded)?;
     match wrapper.algorithm {
+        Algorithm::AesGcm => unwrap_aes_gcm(jwt, &wrapper),
         Algorithm::ChaCha20 => unwrap_chacha20_poly1305(jwt, &wrapper),
         _ => Err(Error::UnsupportedAlgorithm(format!(
             "{}",
@@ -262,6 +316,7 @@ mod test {
         "cL9Vdf_wY_Swv7hOltmRczguEkna0v00vj0p7bGHqhX2uKpuavRGsdgNs1A0CgvaAKnt49NFm9iyt",
         "Cjq3vm0Rmcu_Eg84ZY01LA828WZIcQ",
     );
+    const TEST_SECRET: &str = "shhh - I'm hunting rabbits!";
 
     #[test]
     fn algorithm_to_string() {
@@ -413,6 +468,16 @@ mod test {
         let salt: &[u8] = &[];
 
         assert_eq!(expected, generate_key(&icm, Some(salt), Some(128)).unwrap());
+
+        // another test here to make sure we have a default key length
+        let result = generate_key(&icm, Some(salt), None).unwrap();
+        assert_eq!(result.len(), KEY_LEN);
+        assert_eq!(&expected[0..KEY_LEN], result);
+
+        // verify salt is generated
+        let result = generate_key(&icm, None, Some(128)).unwrap();
+        assert_eq!(result.len(), 128);
+        assert_ne!(expected, result);
     }
 
     #[test]
@@ -427,9 +492,6 @@ mod test {
     fn wrap_unsupported() {
         let jwt = b"fake-jwt-key";
         let plaintext = "this is sample plaintext";
-        let result = wrap(Algorithm::AesGcm, jwt, plaintext.as_bytes()).unwrap_err();
-        assert_eq!(result, Error::UnsupportedAlgorithm(AES_GCM_STR.to_string()));
-
         let result = wrap(Algorithm::Unknown, jwt, plaintext.as_bytes()).unwrap_err();
         assert_eq!(result, Error::UnsupportedAlgorithm(UNKNOWN_STR.to_string()));
     }
@@ -440,11 +502,6 @@ mod test {
         let nonce = "sample_nonce";
         let ciphertext = "cipher_text_goes_here";
         let tag = "tag_value_goes_here";
-        let algo_str = AES_GCM_STR.to_string();
-        let encoded_string = format!("smaash:{}:{}:{}:{}", algo_str, nonce, ciphertext, tag);
-        let result = unwrap(jwt, encoded_string.as_str()).unwrap_err();
-        assert_eq!(result, Error::UnsupportedAlgorithm(algo_str));
-
         let algo_str = UNKNOWN_STR.to_string();
         let encoded_string = format!("smaash:{}:{}:{}:{}", algo_str, nonce, ciphertext, tag);
         let result = unwrap(jwt, encoded_string.as_str()).unwrap_err();
@@ -453,20 +510,47 @@ mod test {
 
     #[test]
     fn chacha20_known_answer() {
-        let secret = "shhh - I'm hunting rabbits!";
         // this was generated by vaas server code, so make sure we can decrypt it
-        let wrapped = "smaash:chacha20:ynDA/nRqArJnn6Jo:dogot5FHY7AFGe6CjAJbqzGFb9mOpEUUk8w2TyRmzKBOYj6O:pzoMYaFOqzRXFo2ToaMEGA==";
+        let wrapped = "smaash:chacha20:082zVRh/NfzCSMyb:RFYIyf1J/yTRdKZKwrG35o8BMV6OqeXUjoHRqbDxEVzRZwaO:8kzWn7kXU6aPw1y5Q4hoHw==";
         let unwrapped = unwrap(TEST_JWT.as_bytes(), wrapped).unwrap();
         let decoded = base64::decode(&unwrapped).unwrap();
-        assert_eq!(secret.as_bytes(), decoded);
+        assert_eq!(TEST_SECRET.as_bytes(), decoded);
     }
 
     #[test]
     fn chacha20_wrap_and_unwrap() {
-        let secret = "shhh - I'm hunting rabbits!";
-        let wrapped = wrap(Algorithm::ChaCha20, TEST_JWT.as_bytes(), secret.as_bytes()).unwrap();
+        let wrapped = wrap(
+            Algorithm::ChaCha20,
+            TEST_JWT.as_bytes(),
+            TEST_SECRET.as_bytes(),
+        )
+        .unwrap();
+        assert!(wrapped.contains(CHA_CHA_20_STR));
         let unwrapped = unwrap(TEST_JWT.as_bytes(), wrapped.as_str()).unwrap();
         let result = std::str::from_utf8(&unwrapped).unwrap();
-        assert_eq!(secret, result);
+        assert_eq!(TEST_SECRET, result);
+    }
+
+    #[test]
+    fn aes_gcm_known_answer() {
+        // this was generated by vaas server code, so make sure we can decrypt it
+        let wrapped = "smaash:aes_gcm:+CFANT6cgczDMH1X:3H5W3RZ4XZUt5Jkm81Z50NmC4SvIxKLFRtEx2yvMiKd/OihU:4CrkcHCcaW8nOSy60RZsCw==";
+        let unwrapped = unwrap(TEST_JWT.as_bytes(), wrapped).unwrap();
+        let decoded = base64::decode(&unwrapped).unwrap();
+        assert_eq!(TEST_SECRET.as_bytes(), decoded);
+    }
+
+    #[test]
+    fn aes_gcm_wrap_and_unwrap() {
+        let wrapped = wrap(
+            Algorithm::AesGcm,
+            TEST_JWT.as_bytes(),
+            TEST_SECRET.as_bytes(),
+        )
+        .unwrap();
+        assert!(wrapped.contains(AES_GCM_STR));
+        let unwrapped = unwrap(TEST_JWT.as_bytes(), wrapped.as_str()).unwrap();
+        let result = std::str::from_utf8(&unwrapped).unwrap();
+        assert_eq!(TEST_SECRET, result);
     }
 }
