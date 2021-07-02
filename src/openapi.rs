72db67d8723a847711cc7bf1da66526fb3875f2b
Rick Porter <rick.porter@cloudtruth.com>
2021-07-02 12:46:04 -0400

    OpenApi client cleanup: cli/client use same 'reqwest' version, add OS to user-agent, turn OpenApiConfig into a singleton, put API key into bearer token

--- src/openapi.rs
+++ src/openapi.rs
@@ -1,12 +1,16 @@
 use crate::config::Config as CloudTruthConfig;
 
-use cloudtruth_restapi::apis::configuration::{ApiKey, Configuration};
+use cloudtruth_restapi::apis::configuration::Configuration;
+use once_cell::sync::OnceCell;
+use std::env;
 
 pub type OpenApiConfig = Configuration;
 
+static INSTANCE: OnceCell<OpenApiConfig> = OnceCell::new();
+
 fn user_agent_name() -> String {
     format!(
-        "{}/{}",
+        "{}/{}/{}",
         option_env!("CARGO_PKG_NAME")
             .unwrap_or("cloudtruth")
             .to_string(),
@@ -14,28 +18,30 @@ fn user_agent_name() -> String {
         option_env!("CARGO_PKG_VERSION")
             .unwrap_or("99.99.99")
             .to_string(),
+        env::consts::OS,
     )
 }
 
-impl From<&CloudTruthConfig> for OpenApiConfig {
-    fn from(ct_cfg: &CloudTruthConfig) -> Self {
-        // TODO: use reqwest builder to create client with a timeout?
-        let mut result = OpenApiConfig::new();
-        result.base_path = ct_cfg.server_url.clone();
-        result.user_agent = Some(user_agent_name());
-        result.api_key = Some(ApiKey {
-            prefix: None,
-            key: ct_cfg.api_key.clone(),
-        });
-        result
+fn create_openapi_config(ct_cfg: &CloudTruthConfig) -> OpenApiConfig {
+    OpenApiConfig {
+        base_path: ct_cfg.server_url.clone(),
+        user_agent: Some(user_agent_name()),
+        client: reqwest::Client::builder()
+            .timeout(ct_cfg.request_timeout.unwrap())
+            .build()
+            .unwrap(),
+        basic_auth: None,
+        oauth_access_token: None,
+        bearer_access_token: Some(ct_cfg.api_key.clone()),
+        api_key: None,
     }
 }
 
 #[allow(dead_code)]
 /// Converts the global CloudTruth config (`config::Config`) into a REST API config
 /// (`cloudtruth_restapi::apis::configuration::Configuration`).
-pub fn open_api_config() -> OpenApiConfig {
-    OpenApiConfig::from(CloudTruthConfig::global())
+pub fn open_api_config() -> &'static OpenApiConfig {
+    INSTANCE.get_or_init(|| create_openapi_config(CloudTruthConfig::global()))
 }
 
 #[cfg(test)]
@@ -56,12 +62,16 @@ mod tests {
             environment: Some("my-env".to_string()),
             project: Some("my-proj".to_string()),
             server_url: url.to_string(),
-            request_timeout: Some(Duration::new(30, 0)),
+            request_timeout: Some(Duration::new(120, 0)),
         };
-        let openapi_cfg = OpenApiConfig::from(&ct_cfg);
+        let openapi_cfg = create_openapi_config(&ct_cfg);
         assert_eq!(openapi_cfg.base_path, url.to_string());
-        assert_eq!(openapi_cfg.api_key.unwrap().key, api_key.to_string());
+        assert_eq!(
+            openapi_cfg.bearer_access_token.unwrap(),
+            api_key.to_string()
+        );
         assert_eq!(openapi_cfg.user_agent.unwrap(), user_agent_name());
+        // unfortunately, no means to interrogate the client to find the timeout
     }
 
     #[test]
@@ -77,8 +87,11 @@ mod tests {
         );
         let openapi_cfg = open_api_config();
         assert_eq!(openapi_cfg.base_path, url.to_string());
-        assert_eq!(openapi_cfg.api_key.unwrap().key, api_key.to_string());
-        assert_eq!(openapi_cfg.user_agent.unwrap(), user_agent_name());
+        assert_eq!(
+            openapi_cfg.bearer_access_token.clone().unwrap(),
+            api_key.to_string()
+        );
+        assert_eq!(openapi_cfg.user_agent.clone().unwrap(), user_agent_name());
 
         env::remove_var(CT_API_KEY);
         env::remove_var(CT_SERVER_URL);
