f60c21cb04ff955af26e39f5714e8d5edb439109
Rick Porter <rick.porter@cloudtruth.com>
2021-08-06 12:33:27 -0400

    Improve code readability and maintainability (#144)
    1. Initial move of process_x_command into different files
    2. Breakup process_x_command() functions into more maintainable pieces

---
+++ src/run.rs
@@ -0,0 +1,73 @@
+use crate::database::{OpenApiConfig, Parameters};
+use crate::subprocess::{EnvSettings, Inheritance, SubProcess};
+use crate::{
+    format_param_error, warn_missing_subcommand, warn_unresolved_params, warn_user, ResolvedIds,
+};
+use clap::ArgMatches;
+use color_eyre::eyre::Result;
+use std::process;
+use std::str::FromStr;
+
+/// Process the 'run' sub-command
+pub fn process_run_command(
+    subcmd_args: &ArgMatches,
+    rest_cfg: &OpenApiConfig,
+    sub_proc: &mut SubProcess,
+    resolved: &ResolvedIds,
+) -> Result<()> {
+    let mut arguments: Vec<String>;
+    let command: String;
+
+    let parameters = Parameters::new();
+    let param_map = parameters.get_parameter_values(
+        rest_cfg,
+        resolved.project_id(),
+        resolved.environment_id(),
+        false,
+    )?;
+    let mut ct_vars = EnvSettings::new();
+    let mut errors: Vec<String> = vec![];
+    for (k, v) in param_map {
+        ct_vars.insert(k.clone(), v.value.clone());
+        if !v.error.is_empty() {
+            errors.push(format_param_error(&k, &v.error))
+        }
+    }
+    sub_proc.set_cloudtruth_environment(ct_vars);
+
+    if subcmd_args.is_present("command") {
+        command = subcmd_args.value_of("command").unwrap().to_string();
+        arguments = vec![];
+    } else if subcmd_args.is_present("arguments") {
+        arguments = subcmd_args.values_of_lossy("arguments").unwrap();
+        command = arguments.remove(0);
+        if command.contains(' ') {
+            warn_user("command contains spaces, and may fail.".to_string())?;
+            let mut reformed = format!("{} {}", command, arguments.join(" "));
+            reformed = reformed.replace("$", "\\$");
+            println!(
+                "Try using 'cloudtruth run --command \"{}\"'",
+                reformed.trim()
+            );
+        }
+    } else {
+        warn_missing_subcommand("run")?;
+        process::exit(0);
+    }
+
+    // NOTE: do this before running the sub-process, since it could be a long-running task
+    warn_unresolved_params(&errors)?;
+
+    // Setup the environment for the sub-process.
+    let inherit = Inheritance::from_str(subcmd_args.value_of("inheritance").unwrap()).unwrap();
+    let overrides = subcmd_args.values_of_lossy("set").unwrap_or_default();
+    let removals = subcmd_args.values_of_lossy("remove").unwrap_or_default();
+    let permissive = subcmd_args.is_present("permissive");
+    sub_proc.set_environment(resolved, inherit, &overrides, &removals)?;
+    if !permissive {
+        sub_proc.remove_ct_app_vars();
+    }
+    sub_proc.run_command(command.as_str(), &arguments)?;
+
+    Ok(())
+}
---
+++ src/templates.rs
@@ -0,0 +1,199 @@
+use crate::cli::{
+    CONFIRM_FLAG, DELETE_SUBCMD, FORMAT_OPT, GET_SUBCMD, LIST_SUBCMD, NAME_ARG, RENAME_OPT,
+    SECRETS_FLAG, SET_SUBCMD, TEMPLATE_FILE_OPT, VALUES_FLAG,
+};
+use crate::database::{OpenApiConfig, Templates};
+use crate::table::Table;
+use crate::{
+    error_message, user_confirm, warn_missing_subcommand, warning_message, ResolvedIds,
+    FILE_READ_ERR,
+};
+use clap::ArgMatches;
+use color_eyre::eyre::Result;
+use std::fs;
+use std::process;
+
+fn proc_template_delete(
+    subcmd_args: &ArgMatches,
+    rest_cfg: &OpenApiConfig,
+    templates: &Templates,
+    resolved: &ResolvedIds,
+) -> Result<()> {
+    let proj_name = resolved.project_display_name();
+    let proj_id = resolved.project_id();
+    let template_name = subcmd_args.value_of(NAME_ARG).unwrap();
+    let details = templates.get_details_by_name(rest_cfg, proj_id, template_name)?;
+
+    if let Some(details) = details {
+        let mut confirmed = subcmd_args.is_present(CONFIRM_FLAG);
+        if !confirmed {
+            confirmed = user_confirm(format!(
+                "Delete template '{}' in project '{}'",
+                template_name, proj_name
+            ));
+        }
+
+        if !confirmed {
+            warning_message(format!(
+                "Template '{}' in project '{}' not deleted!",
+                template_name, proj_name
+            ))?;
+        } else {
+            templates.delete_template(rest_cfg, proj_id, &details.id)?;
+            println!(
+                "Deleted template '{}' in project '{}'",
+                template_name, proj_name
+            );
+        }
+    } else {
+        warning_message(format!(
+            "Template '{}' does not exist for project '{}'!",
+            template_name, proj_name
+        ))?;
+    }
+    Ok(())
+}
+
+fn proc_template_list(
+    subcmd_args: &ArgMatches,
+    rest_cfg: &OpenApiConfig,
+    templates: &Templates,
+    resolved: &ResolvedIds,
+) -> Result<()> {
+    let proj_name = resolved.project_display_name();
+    let proj_id = resolved.project_id();
+    let details = templates.get_template_details(rest_cfg, proj_id)?;
+    if details.is_empty() {
+        println!("No templates in project '{}'.", proj_name);
+    } else if !subcmd_args.is_present(VALUES_FLAG) {
+        let list = details
+            .iter()
+            .map(|n| n.name.clone())
+            .collect::<Vec<String>>();
+        println!("{}", list.join("\n"))
+    } else {
+        let fmt = subcmd_args.value_of(FORMAT_OPT).unwrap();
+        let mut table = Table::new("template");
+        table.set_header(&["Name", "Description"]);
+        for entry in details {
+            table.add_row(vec![entry.name, entry.description]);
+        }
+        table.render(fmt)?;
+    }
+    Ok(())
+}
+
+fn proc_template_get(
+    subcmd_args: &ArgMatches,
+    rest_cfg: &OpenApiConfig,
+    templates: &Templates,
+    resolved: &ResolvedIds,
+) -> Result<()> {
+    let proj_name = resolved.project_display_name();
+    let proj_id = resolved.project_id();
+    let env_id = resolved.environment_id();
+    let template_name = subcmd_args.value_of(NAME_ARG).unwrap();
+    let show_secrets = subcmd_args.is_present(SECRETS_FLAG);
+    let body =
+        templates.get_body_by_name(rest_cfg, proj_id, env_id, template_name, show_secrets)?;
+
+    if let Some(body) = body {
+        println!("{}", body)
+    } else {
+        error_message(format!(
+            "No template '{}' found in project '{}'.",
+            template_name, proj_name
+        ))?;
+        process::exit(9);
+    }
+    Ok(())
+}
+
+fn proc_template_preview(
+    subcmd_args: &ArgMatches,
+    rest_cfg: &OpenApiConfig,
+    templates: &Templates,
+    resolved: &ResolvedIds,
+) -> Result<()> {
+    let proj_id = resolved.project_id();
+    let env_id = resolved.environment_id();
+    let show_secrets = subcmd_args.is_present(SECRETS_FLAG);
+    let filename = subcmd_args.value_of(TEMPLATE_FILE_OPT).unwrap();
+    let body = fs::read_to_string(filename).expect(FILE_READ_ERR);
+    let result = templates.preview_template(rest_cfg, proj_id, env_id, &body, show_secrets)?;
+    println!("{}", result);
+    Ok(())
+}
+
+fn proc_template_set(
+    subcmd_args: &ArgMatches,
+    rest_cfg: &OpenApiConfig,
+    templates: &Templates,
+    resolved: &ResolvedIds,
+) -> Result<()> {
+    let filename = subcmd_args.value_of(TEMPLATE_FILE_OPT);
+    let proj_id = resolved.project_id();
+    let proj_name = resolved.project_display_name();
+    let template_name = subcmd_args.value_of(NAME_ARG).unwrap();
+    let rename = subcmd_args.value_of(RENAME_OPT);
+    let description = subcmd_args.value_of("description");
+    let details = templates.get_details_by_name(rest_cfg, proj_id, template_name)?;
+
+    if let Some(details) = details {
+        if description.is_none() && rename.is_none() && filename.is_none() {
+            warning_message(format!(
+                "Template '{}' not updated: no updated parameters provided",
+                template_name
+            ))?;
+        } else {
+            let name = rename.unwrap_or(&template_name);
+            let mut body = None;
+            if let Some(filename) = filename {
+                body = Some(fs::read_to_string(filename).expect(FILE_READ_ERR));
+            }
+            templates.update_template(
+                rest_cfg,
+                proj_id,
+                &details.id,
+                name,
+                description,
+                body.as_deref(),
+            )?;
+            println!("Updated template '{}' in project '{}'", name, proj_name);
+        }
+    } else if let Some(filename) = filename {
+        let body = fs::read_to_string(filename).expect(FILE_READ_ERR);
+        templates.create_template(rest_cfg, proj_id, template_name, &body, description)?;
+        println!(
+            "Created template '{}' in project '{}'",
+            template_name, proj_name
+        );
+    } else {
+        error_message("Must provide a body for a new template".to_owned())?;
+        process::exit(8);
+    }
+    Ok(())
+}
+
+/// Process the 'templates' sub-command
+pub fn process_templates_command(
+    subcmd_args: &ArgMatches,
+    rest_cfg: &OpenApiConfig,
+    templates: &Templates,
+    resolved: &ResolvedIds,
+) -> Result<()> {
+    if let Some(subcmd_args) = subcmd_args.subcommand_matches(DELETE_SUBCMD) {
+        proc_template_delete(subcmd_args, rest_cfg, templates, resolved)?;
+    } else if let Some(subcmd_args) = subcmd_args.subcommand_matches(LIST_SUBCMD) {
+        proc_template_list(subcmd_args, rest_cfg, templates, resolved)?;
+    } else if let Some(subcmd_args) = subcmd_args.subcommand_matches(GET_SUBCMD) {
+        proc_template_get(subcmd_args, rest_cfg, templates, resolved)?;
+    } else if let Some(subcmd_args) = subcmd_args.subcommand_matches("preview") {
+        proc_template_preview(subcmd_args, rest_cfg, templates, resolved)?;
+    } else if let Some(subcmd_args) = subcmd_args.subcommand_matches(SET_SUBCMD) {
+        proc_template_set(subcmd_args, rest_cfg, templates, resolved)?;
+    } else {
+        warn_missing_subcommand("templates")?;
+    }
+    Ok(())
+}
