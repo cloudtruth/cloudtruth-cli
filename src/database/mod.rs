3547e2375d1c0beac9ae845ff3d526e8d66728e4
Rick Porter <rick.porter@cloudtruth.com>
2021-09-21 14:14:32 -0400

    Split parameter type and rule definitions into separate modules

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -2,6 +2,8 @@ mod environments;
 mod history;
 mod integrations;
 mod openapi;
+mod parameter_rules;
+mod parameter_types;
 mod parameters;
 mod projects;
 mod templates;
@@ -10,9 +12,11 @@ pub use environments::{EnvironmentDetails, EnvironmentError, EnvironmentUrlMap, 
 pub use history::HistoryAction;
 pub use integrations::{IntegrationDetails, IntegrationError, Integrations};
 pub use openapi::OpenApiConfig;
+pub use parameter_rules::{ParamRuleType, ParameterDetailRule};
+pub use parameter_types::ParamType;
 pub use parameters::{
-    ParamExportFormat, ParamExportOptions, ParamRuleType, ParamType, ParameterDetailMap,
-    ParameterDetails, ParameterError, Parameters,
+    ParamExportFormat, ParamExportOptions, ParameterDetailMap, ParameterDetails, ParameterError,
+    Parameters,
 };
 pub use projects::{ProjectDetails, ProjectError, Projects};
 pub use templates::{TemplateDetails, TemplateHistory, Templates};
