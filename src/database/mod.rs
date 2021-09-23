0e14085ca30409e60204a2e07922859b20f43f16
Rick Porter <rick.porter@cloudtruth.com>
2021-09-23 10:10:28 -0400

    Move ParameterDetails into another module

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -2,6 +2,7 @@ mod environments;
 mod history;
 mod integrations;
 mod openapi;
+mod parameter_details;
 mod parameter_rules;
 mod parameter_types;
 mod parameters;
@@ -12,11 +13,11 @@ pub use environments::{EnvironmentDetails, EnvironmentError, EnvironmentUrlMap, 
 pub use history::HistoryAction;
 pub use integrations::{IntegrationDetails, IntegrationError, Integrations};
 pub use openapi::OpenApiConfig;
+pub use parameter_details::ParameterDetails;
 pub use parameter_rules::{ParamRuleType, ParameterDetailRule};
 pub use parameter_types::ParamType;
 pub use parameters::{
-    ParamExportFormat, ParamExportOptions, ParameterDetailMap, ParameterDetails, ParameterError,
-    Parameters,
+    ParamExportFormat, ParamExportOptions, ParameterDetailMap, ParameterError, Parameters,
 };
 pub use projects::{ProjectDetails, ProjectError, Projects};
 pub use templates::{TemplateDetails, TemplateHistory, Templates};
