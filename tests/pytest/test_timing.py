31711fef89e581712523c918e3e412f8a95dd4f5
Rick Porter <rick.porter@cloudtruth.com>
2021-10-06 09:49:42 -0400

    Add simple timing test for various 'template get' options

--- tests/pytest/test_timing.py
+++ tests/pytest/test_timing.py
@@ -11,11 +11,12 @@ from testcase import CT_REST_DEBUG
 # some environment variables for test control (without file modification)
 CT_PERSIST = "CLOUDTRUTH_TEST_PERSIST"
 CT_PARAM_COUNT = "CLOUDTRUTH_TEST_PARAMETER_COUNT"
-
+CT_TEMPLATE_COUNT = "CLOUDTRUTH_TEST_TEMPLATE_COUNT"
 
 ENV_RESOLVE = "env-resolve"
 PROJ_RESOLVE = "proj-resolve"
-DEFAULT_PARAM_COUNT = 20
+DEFAULT_PARAM_COUNT = 10
+DEFAULT_TEMPLATE_COUNT = 5
 
 
 def parse_time(value: str) -> int:
@@ -60,6 +61,7 @@ class TestTiming(TestCase):
         self.leave_up = CT_PERSIST in os.environ
         self.param_count = int(os.environ.get(CT_PARAM_COUNT) or DEFAULT_PARAM_COUNT)
         self.param_prefix = "param"
+        self.template_count = int(os.environ.get(CT_TEMPLATE_COUNT) or DEFAULT_TEMPLATE_COUNT)
         super().setUp()
 
     def _param_name(self, index: int) -> str:
@@ -136,3 +138,86 @@ class TestTiming(TestCase):
         # cleanup
         if not self.leave_up:
             self.delete_project(cmd_env, proj_name)
+
+    def test_timing_template(self) -> None:
+        cmd_env = self.get_cmd_env()
+        base_cmd = self.get_cli_base_cmd()
+        proj_name = self.make_name("test-timing-template")
+        self.create_project(cmd_env, proj_name)
+        env_name = self.make_name("template-timing-env")
+        self.create_environment(cmd_env, env_name)
+
+        proj_cmd = base_cmd + f"--project '{proj_name}' --env '{env_name}' "
+
+        # create a couple parameters
+        param1 = "param1"
+        value1a = "first value"
+        param2 = "param2"
+        value2a = "another first"
+        self.set_param(cmd_env, proj_name, param1, value=value1a, env=env_name)
+        self.set_param(cmd_env, proj_name, param2, value=value2a, env=env_name)
+
+        filename = "temp-timing.txt"
+        body = """\
+PARAMETER1=PARAM1
+PARAMETER2=PARAM2
+"""
+        raw_body = body.replace("PARAM1", f"{{{{{param1}}}}}").replace("PARAM2", f"{{{{{param2}}}}}")
+        self.write_file(filename, raw_body)
+        temp_name = "timing-template"
+        result = self.run_cli(cmd_env, proj_cmd + f"template set '{temp_name}' -b {filename}")
+        self.assertResultSuccess(result)
+
+        # tag the environment
+        tag_name = "my-temp-timing-tag"
+        result = self.run_cli(cmd_env, base_cmd + f"env tag set '{env_name}' '{tag_name}'")
+        self.assertResultSuccess(result)
+
+        # update the values
+        value1b = "second value"
+        value2b = "first loser"
+        self.set_param(cmd_env, proj_name, param1, value=value1b, env=env_name)
+        self.set_param(cmd_env, proj_name, param2, value=value2b, env=env_name)
+
+        eval_body_a = body.replace("PARAM1", f"{value1a}").replace("PARAM2", f"{value2a}")
+        eval_body_b = body.replace("PARAM1", f"{value1b}").replace("PARAM2", f"{value2b}")
+
+        temp_get_raw_current = []
+        temp_get_current = []
+        temp_get_tag = []
+        temp_get_raw_tag = []
+
+        temp_get = proj_cmd + f"template get '{temp_name}' "
+
+        for _ in range(self.template_count):
+            result = self.run_cli(cmd_env, temp_get + f"--as-of '{tag_name}' --raw")
+            self.assertResultSuccess(result)
+            self.assertEqual(result.out(), raw_body)
+            temp_get_raw_current.append(delta_to_msecs(result.timediff))
+
+            result = self.run_cli(cmd_env, temp_get)
+            self.assertResultSuccess(result)
+            self.assertEqual(result.out(), eval_body_b)
+            temp_get_current.append(delta_to_msecs(result.timediff))
+
+            result = self.run_cli(cmd_env, temp_get + f"--as-of '{tag_name}'")
+            self.assertResultSuccess(result)
+            self.assertEqual(result.out(), eval_body_a)
+            temp_get_tag.append(delta_to_msecs(result.timediff))
+
+            result = self.run_cli(cmd_env, temp_get + f"--as-of '{tag_name}' --raw")
+            self.assertResultSuccess(result)
+            self.assertEqual(result.out(), raw_body)
+            temp_get_raw_tag.append(delta_to_msecs(result.timediff))
+
+        timing_info = OrderedDict()
+        timing_info["template-get-raw"] = temp_get_raw_current
+        timing_info["template-get-raw-tag"] = temp_get_raw_tag
+        timing_info["template-get-current"] = temp_get_current
+        timing_info["template-get-tag"] = temp_get_tag
+        print_timing_info("test_timing_template", timing_info)
+
+        # cleanup
+        self.delete_file(filename)
+        self.delete_environment(cmd_env, env_name)
+        self.delete_project(cmd_env, proj_name)
