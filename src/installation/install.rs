fc14ca3cdf021d03773137ff01e2135911bcf2ff
Rick Porter <rick.porter@cloudtruth.com>
2021-12-05 08:46:02 -0500

    Adjustments for Windoz

--- src/installation/install.rs
+++ src/installation/install.rs
@@ -2,26 +2,33 @@ use crate::installation::{binary_version, InstallError};
 use std::fs;
 use std::io;
 use std::io::Write;
-use std::os::unix::fs::PermissionsExt;
 use std::process::Command;
 use std::str;
 use tempfile::tempdir;
 
 #[cfg(target_os = "windows")]
-const INSTALL_TEXT: &[u8] = include_bytes!(".,/../install.ps1");
+const INSTALL_TEXT: &[u8] = include_bytes!("../../install.ps1");
 
 #[cfg(not(target_os = "windows"))]
 const INSTALL_TEXT: &[u8] = include_bytes!("../../install.sh");
 
+#[cfg(target_os = "windows")]
+const EXTENSION: &str = "ps1";
+
+#[cfg(not(target_os = "windows"))]
+const EXTENSION: &str = "sh";
+
 pub fn install_latest_version(quiet: bool) -> Result<(), InstallError> {
-    let filename = format!("cloudtruth-cli-install-{}", binary_version());
+    let filename = format!("cloudtruth-cli-install-{}.{}", binary_version(), EXTENSION);
     let tempdir = tempdir()?;
     let fullpath = tempdir.path().join(filename);
     let fullname = fullpath.to_str().unwrap();
 
     // write the install script to a file to a temporary directory
     fs::write(fullname, INSTALL_TEXT)?;
-    fs::set_permissions(fullname, std::fs::Permissions::from_mode(0o755))?;
+    if !cfg!(windows) {
+        let _ = Command::new("chmod").arg("a+x").arg(fullname).output();
+    }
 
     // now, actually run the installation script
     let result = Command::new(fullname).output();
