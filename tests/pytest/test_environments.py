88ca129f144d31299e19effc6e5fcd16e006099b
Rick Porter <rick.porter@cloudtruth.com>
2022-01-27 09:19:23 -0500

    Consolidate eval usage in tests

--- tests/pytest/test_environments.py
+++ tests/pytest/test_environments.py
@@ -245,9 +245,8 @@ class TestEnvironments(TestCase):
 
         # make sure we can see the entry, before it has been read
         usage_cmd = list_cmd + "-uf json"
-        result = self.run_cli(cmd_env, usage_cmd)
-        self.assertResultSuccess(result)
-        tag_data = eval(result.out()).get("environment-tags")[0]
+        tags = self.get_cli_entries(cmd_env, usage_cmd, "environment-tags")
+        tag_data = tags[0]
         self.assertEqual(tag1, tag_data.get("Name"))
         self.assertEqual(desc1b, tag_data.get("Description"))  # updated
         self.assertEqual(orig_time, tag_data.get("Timestamp"))  # not updated
@@ -269,9 +268,8 @@ class TestEnvironments(TestCase):
         self.assertNotIn(value1, result.out())
 
         # see the tag usage is updated
-        result = self.run_cli(cmd_env, usage_cmd)
-        self.assertResultSuccess(result)
-        tag_data = eval(result.out()).get("environment-tags")[0]
+        tags = self.get_cli_entries(cmd_env, usage_cmd, "environment-tags")
+        tag_data = tags[0]
         self.assertEqual("2", tag_data.get("Total Reads"))
         self.assertNotEqual("", tag_data.get("Last User"))
         self.assertNotEqual("", tag_data.get("Last Time"))
@@ -281,9 +279,8 @@ class TestEnvironments(TestCase):
         self.assertResultSuccess(result)
 
         usage_cmd = list_cmd + "-uf json"
-        result = self.run_cli(cmd_env, usage_cmd)
-        self.assertResultSuccess(result)
-        tag_data = eval(result.out()).get("environment-tags")[0]
+        tags = self.get_cli_entries(cmd_env, usage_cmd, "environment-tags")
+        tag_data = tags[0]
         self.assertEqual(tag1, tag_data.get("Name"))
         self.assertEqual(desc1b, tag_data.get("Description"))
         self.assertLess(orig_time, tag_data.get("Timestamp"))  # update time should be newer
