aa787ebb60f9a976b488d23f93393bb30ddc5a71
Adam Curtis <kallisti.dev@gmail.com>
2023-02-06 18:37:31 -0500

    regenerator openAPI client for history date and user

--- client/src/models/parameter_timeline_entry.rs
+++ client/src/models/parameter_timeline_entry.rs
@@ -14,6 +14,10 @@
 pub struct ParameterTimelineEntry {
     #[serde(rename = "history_type")]
     pub history_type: Option<Box<crate::models::HistoryTypeEnum>>,
+    #[serde(rename = "modified_at")]
+    pub modified_at: Option<String>,
+    #[serde(rename = "modified_by", skip_serializing_if = "Option::is_none")]
+    pub modified_by: Option<String>,
     /// The affected environment(s).
     #[serde(rename = "history_environments")]
     pub history_environments: Vec<crate::models::ParameterTimelineEntryEnvironment>,
@@ -29,12 +33,15 @@ impl ParameterTimelineEntry {
     /// Details about a single change.
     pub fn new(
         history_type: Option<crate::models::HistoryTypeEnum>,
+        modified_at: Option<String>,
         history_environments: Vec<crate::models::ParameterTimelineEntryEnvironment>,
         history_model: Option<crate::models::HistoryModelEnum>,
         history_parameter: Option<crate::models::ParameterTimelineEntryParameter>,
     ) -> ParameterTimelineEntry {
         ParameterTimelineEntry {
             history_type: history_type.map(Box::new),
+            modified_at,
+            modified_by: None,
             history_environments,
             history_model: history_model.map(Box::new),
             history_parameter: history_parameter.map(Box::new),
