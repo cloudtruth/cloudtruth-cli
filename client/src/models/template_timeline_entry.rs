65338c178e2ed16b902f7887bb255fefa184b5aa
Adam Curtis <adam.curtis@cloudtruth.com>
2023-02-06 20:33:28 -0500

    regenerator openAPI client for history date and user (#411)

--- client/src/models/template_timeline_entry.rs
+++ client/src/models/template_timeline_entry.rs
@@ -14,6 +14,10 @@
 pub struct TemplateTimelineEntry {
     #[serde(rename = "history_type")]
     pub history_type: Option<Box<crate::models::HistoryTypeEnum>>,
+    #[serde(rename = "modified_at")]
+    pub modified_at: Option<String>,
+    #[serde(rename = "modified_by", skip_serializing_if = "Option::is_none")]
+    pub modified_by: Option<String>,
     /// The template record as it was when archived for history.
     #[serde(rename = "history_template")]
     pub history_template: Option<Box<crate::models::TemplateTimelineEntryTemplate>>,
@@ -23,10 +27,13 @@ impl TemplateTimelineEntry {
     /// Details about a single change.
     pub fn new(
         history_type: Option<crate::models::HistoryTypeEnum>,
+        modified_at: Option<String>,
         history_template: Option<crate::models::TemplateTimelineEntryTemplate>,
     ) -> TemplateTimelineEntry {
         TemplateTimelineEntry {
             history_type: history_type.map(Box::new),
+            modified_at,
+            modified_by: None,
             history_template: history_template.map(Box::new),
         }
     }
