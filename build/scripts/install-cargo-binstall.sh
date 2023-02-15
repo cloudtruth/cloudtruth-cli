16c30c3b8339ec515a625eac9f84e7255f58cd7b
Adam Curtis <kallisti.dev@gmail.com>
2023-02-15 13:20:40 -0500

    remove build-cli-action from workflows

---
+++ build/scripts/install-cargo-binstall.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+BASE_BINSTALL_URL='https://github.com/cargo-bins/cargo-binstall/releases/latest/download/'
+case ${{ runner.os }} in
+    Linux)
+        BINSTALL_FILE='cargo-binstall-x86_64-unknown-linux-musl.tgz'
+        BINSTALL_URL="$BASE_BINSTALL_URL/$BINSTALL_FILE"
+        curl -LSs "$BINSTALL_URL" | tar -C $HOME/.cargo/bin -xvz
+    ;;
+    macOS)
+        BINSTALL_FILE='cargo-binstall-universal-apple-darwin.zip'
+        BINSTALL_URL="$BASE_BINSTALL_URL/$BINSTALL_FILE"
+        curl -LSs "$BINSTALL_URL" > "/tmp/$BINSTALL_FILE"
+        unzip -o -d "$HOME/.cargo/bin" "/tmp/$BINSTALL_FILE"
+    ;;
+    Windows)
+        BINSTALL_FILE='cargo-binstall-x86_64-pc-windows-msvc.zip'
+        BINSTALL_URL="$BASE_BINSTALL_URL/$BINSTALL_FILE"
+        curl -LSs "$BINSTALL_URL" > "/tmp/$BINSTALL_FILE"
+        unzip -o -d "$HOME/.cargo/bin" "/tmp/$BINSTALL_FILE"
+esac;
