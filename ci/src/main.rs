117eb22c3ae78952e37ac1a5a19e1da9535020bb
Adam Curtis <kallisti.dev@gmail.com>
2023-02-22 22:45:20 -0500

    clean up path handling and error reporting

--- ci/src/main.rs
+++ ci/src/main.rs
@@ -18,56 +18,25 @@ use config::*;
 use docker_template::*;
 use gh_matrix_map::*;
 
+/// Default base path for GH matrix outputs
 macro_rules! matrix_path {
     ($($path:literal),*) => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/gh-actions/", $($path),*)
+        concat!("./gh-actions/", $($path),*)
     };
 }
 
+/// Default base path for docker outputs
 macro_rules! docker_path {
     ($($path:literal),*) => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/docker/", $($path),*)
+        concat!("./docker/", $($path),*)
     };
 }
 
+/// Default path to config file
 macro_rules! config_yaml_path {
     () => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/config.yaml")
+        "./config.yaml"
     };
-}
-
-pub fn open_input_file<P: AsRef<Path>>(path: P, verbose: bool) -> Result<File> {
-    if verbose {
-        println!("Reading from {path}", path = path.as_ref().display());
-    }
-    Ok(File::open(path)?)
-}
-
-/// Helper for opening generated output files
-pub fn open_output_file<P: AsRef<Path>>(path: P, verbose: bool) -> Result<File> {
-    if verbose {
-        println!("Writing to {path}", path = path.as_ref().display());
-    }
-    Ok(File::create(path)?)
-}
-
-pub fn mkdir<P: AsRef<Path>>(path: P) -> Result<()> {
-    if !path.as_ref().is_dir() {
-        create_dir(path)?
-    }
-    Ok(())
-}
-
-pub fn display_matrix_map<K, M>(name: &str, map: &MatrixMap<K, M>)
-where
-    MatrixMap<K, M>: Display,
-{
-    print!("=== Generated matrices for {name} ===\n{map}");
-}
-
-pub fn show_help() {
-    use clap::CommandFactory;
-    Cli::command().print_help().unwrap();
 }
 
 #[derive(Parser)]
@@ -82,16 +51,56 @@ struct Cli {
     docker: bool,
 }
 
+impl Cli {
+    pub fn show_help() {
+        use clap::CommandFactory;
+        Self::command().print_help().unwrap();
+    }
+    pub fn open_input_file<P: AsRef<Path>>(&self, path: P) -> Result<File> {
+        if self.verbose {
+            println!("Reading {}", path.as_ref().display());
+        }
+        File::open(path.as_ref())
+            .with_context(|| format!("Unable to open file for reading: {:?}", path.as_ref()))
+    }
+
+    /// Helper for opening generated output files
+    pub fn open_output_file<P: AsRef<Path>>(&self, path: P) -> Result<File> {
+        if self.verbose {
+            println!("Writing {}", path.as_ref().display());
+        }
+        File::create(path.as_ref())
+            .with_context(|| format!("Unable to open file for writing: {:?}", path.as_ref()))
+    }
+
+    pub fn display_matrix_map<K, M>(&self, name: &str, map: &MatrixMap<K, M>)
+    where
+        MatrixMap<K, M>: Display,
+    {
+        print!("=== Generated matrices for {name} ===\n{map}");
+    }
+
+    pub fn mkdir<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        if !path.as_ref().is_dir() {
+            create_dir(path.as_ref())
+                .with_context(|| format!("Unable to create directory: {:?}", path.as_ref()))?;
+            if self.verbose {
+                println!("Created directory {}", path.as_ref().display())
+            }
+        } else if self.verbose {
+            println!("Found directory {}", path.as_ref().display());
+        }
+        Ok(())
+    }
+}
+
 fn generate_dockerfiles(cli: &Cli, config: &Config) -> Result<()> {
     let docker_base_path = Path::new(docker_path!());
-    mkdir(docker_base_path)?;
+    cli.mkdir(docker_base_path)?;
     let results = DockerTemplate::iter_from_config(&config.release_tests).map(|template| {
         let path = docker_base_path.join(template.file_name());
-        let file = open_output_file(&path, cli.verbose)
-            .with_context(|| format!("Unable to open Dockerfile at {}", path.display()))?;
-        template
-            .write_dockerfile(file)
-            .with_context(|| format!("Unable to write Dockerfile at {}", path.display()))
+        let file = cli.open_output_file(path)?;
+        template.write_dockerfile(file)
     });
     collect_file_errors(
         anyhow!("Multiple file errors when generating Dockerfiles"),
@@ -100,17 +109,17 @@ fn generate_dockerfiles(cli: &Cli, config: &Config) -> Result<()> {
 }
 
 fn generate_actions_matrices<'a: 'b, 'b>(cli: &Cli, config: &'a mut Config<'b>) -> Result<()> {
-    mkdir(matrix_path!())?;
+    cli.mkdir(matrix_path!())?;
     let build_map = BuildMatrixMap::from_config(&mut config.release_builds);
     if cli.verbose {
-        display_matrix_map("build_release", &build_map);
+        cli.display_matrix_map("build_release", &build_map);
     }
     let test_map = TestMatrixMap::from_config(&mut config.release_tests);
     if cli.verbose {
-        display_matrix_map("test_release", &test_map)
+        cli.display_matrix_map("test_release", &test_map)
     };
-    let build_release_file = open_output_file(matrix_path!("build_release.json"), cli.verbose)?;
-    let test_release_file = open_output_file(matrix_path!("test_release.json"), cli.verbose)?;
+    let build_release_file = cli.open_output_file(matrix_path!("build_release.json"))?;
+    let test_release_file = cli.open_output_file(matrix_path!("test_release.json"))?;
     if cli.pretty {
         build_map.write_json_pretty(build_release_file)?;
         test_map.write_json_pretty(test_release_file)?;
@@ -124,21 +133,30 @@ fn generate_actions_matrices<'a: 'b, 'b>(cli: &Cli, config: &'a mut Config<'b>) 
 fn main() -> Result<()> {
     let cli = Cli::parse();
     if !cli.actions && !cli.docker {
-        show_help();
+        Cli::show_help();
         eprintln!("ERROR: One of the following options is required: --actions, --docker");
         std::process::exit(1);
     }
     let config_yaml_path = Path::new(config_yaml_path!());
     let mut config_yaml = String::new();
-    open_input_file(config_yaml_path, cli.verbose)?.read_to_string(&mut config_yaml)?;
+    cli.open_input_file(config_yaml_path)?
+        .read_to_string(&mut config_yaml)?;
     let mut config: Config = serde_yaml::from_str(&config_yaml)?;
+    let mut errors = Vec::new();
     if cli.docker {
-        generate_dockerfiles(&cli, &config)?;
+        if let Err(err) = generate_dockerfiles(&cli, &config) {
+            errors.push(err);
+        }
     }
     if cli.actions {
-        generate_actions_matrices(&cli, &mut config)?;
+        if let Err(err) = generate_actions_matrices(&cli, &mut config) {
+            errors.push(err);
+        }
     }
-    Ok(())
+    collect_file_errors(
+        anyhow!("Multiple file errors when generating CI files"),
+        errors,
+    )
 }
 
 // collects and reports errors
