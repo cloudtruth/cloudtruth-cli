b93f6fc1a4798aab7c5ab904573ab3ae5310d88a
Adam Curtis <kallisti.dev@gmail.com>
2023-02-27 14:59:37 -0500

    fix conditional in install.rust.sh

--- ci/scripts/install-rust.sh
+++ ci/scripts/install-rust.sh
@@ -7,11 +7,11 @@ set -e
 # When rustup is updated, it tries to replace its binary, which on Windows is somehow locked.
 # This can result in the CI failure, see: https://github.com/rust-lang/rustup/issues/3029
 rustup set auto-self-update disable
-# legacy CLI version with no rust-toolchain file
 if [ -f rust-toolchain ] || [ -f rust-toolchain.toml ]; then
     # this command forces install of version in rust-toolchain file
     # this might change in the future but for now it's the best way
     rustup show
 else
+    # legacy CLI version with no rust-toolchain file
     rustup toolchain install 1.63.0 --profile minimal
 fi
