085ea36c33032af3fff1ff8efc8808daca293439
Adam Curtis <kallisti.dev@gmail.com>
2023-02-27 14:53:31 -0500

    fixup env variable handling for asset name

--- ci/scripts/build-release-archive.sh
+++ ci/scripts/build-release-archive.sh
@@ -16,9 +16,11 @@ cp "target/$TARGET/release/$PROJECT_NAME"* "$ARCHIVE_NAME/"
 case $(uname) in
     *Windows*)
         7z a "$ARCHIVE_NAME.zip" "$ARCHIVE_NAME"
+        echo "ASSET=$ARCHIVE_NAME.zip" >> "$GITHUB_ENV"
     ;;
 
     *)
         tar -czf "$ARCHIVE_NAME.tar.gz" "$ARCHIVE_NAME"
+        echo "ASSET=$ARCHIVE_NAME.tar.gz" >> "$GITHUB_ENV"
     ;;
 esac
