8014f900910acc5a0437e28c67ae700fb15f0132
Adam Curtis <kallisti.dev@gmail.com>
2023-04-01 15:53:57 -0400

    Revert "quick prototype of using trycmd for the tests"
    This reverts commit d8d82ffc9e5ba9f2c5942a0a51aa6a187ee1ea1a.

--- tests/test_offline.rs
+++ tests/test_offline.rs
@@ -1,9 +1,13 @@
 use std::path::Path;
-use integration_test_harness::prelude::*;
+
+#[cfg(target_os = "windows")]
+const BIN_PATH: &str = "target/debug/cloudtruth.exe";
+#[cfg(not(target_os = "windows"))]
+const BIN_PATH: &str = "target/debug/cloudtruth";
 
 #[test]
 fn help_text() {
     trycmd::TestCases::new()
-        .register_bin("cloudtruth", Path::new(cli_bin_path!()))
+        .register_bin("cloudtruth", Path::new(BIN_PATH))
         .case("examples/help-text/*.md");
 }
