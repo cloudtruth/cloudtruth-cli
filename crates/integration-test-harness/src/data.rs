97d5b9b2118b8a9fb333afe318783814b7c677b6
Adam Curtis <kallisti.dev@gmail.com>
2023-04-19 15:00:18 -0400

    basic parameter type tests

--- crates/integration-test-harness/src/data.rs
+++ crates/integration-test-harness/src/data.rs
@@ -1,6 +1,7 @@
 pub mod environment;
 pub mod file;
 pub mod name;
+pub mod param_type;
 pub mod project;
 pub mod resource;
 pub mod scope;
@@ -9,6 +10,7 @@ pub mod user;
 pub use environment::*;
 pub use file::*;
 pub use name::*;
+pub use param_type::*;
 pub use project::*;
 pub use resource::*;
 pub use scope::*;
---
+++ crates/integration-test-harness/src/data/param_type.rs
@@ -0,0 +1,85 @@
+use crate::{
+    command::{cli_bin_path, Command},
+    contains,
+    data::{Name, NameConstructors, Scope, TestResource},
+};
+
+#[derive(Clone, Debug, Display)]
+#[display(fmt = "{}", name)]
+pub struct ParamType<'d, 'p> {
+    name: Name,
+    description: Option<&'d str>,
+    parent: Option<&'p Name>,
+}
+
+impl<'d, 'p> ParamType<'d, 'p> {
+    fn new(name: Name, description: Option<&'d str>, parent: Option<&'p Name>) -> Self {
+        Self {
+            name,
+            description,
+            parent,
+        }
+    }
+
+    pub fn rename<N: Into<Name>>(&mut self, name: N) -> &mut Self {
+        let name = name.into();
+        Command::new(cli_bin_path("cloudtruth"))
+            .args([
+                "types",
+                "set",
+                self.name.as_str(),
+                "--rename",
+                name.as_str(),
+            ])
+            .assert()
+            .success()
+            .stdout(contains!("Updated parameter type '{name}'"));
+        self.name = name;
+        self
+    }
+
+    /// Set the parameter types description.
+    pub fn description<D: AsRef<str> + ?Sized>(mut self, description: &'d D) -> Self {
+        self.description = Some(description.as_ref());
+        self
+    }
+
+    /// Set the parameter types parent.
+    pub fn parent(mut self, parent: &'p ParamType) -> Self {
+        self.parent = Some(&parent.name);
+        self
+    }
+}
+
+impl<'d, 'p> NameConstructors for ParamType<'d, 'p> {
+    fn from_name<N: Into<Name>>(name: N) -> Self {
+        Self::new(name.into(), None, None)
+    }
+}
+
+impl<'d, 'p> TestResource for ParamType<'d, 'p> {
+    fn name(&self) -> &Name {
+        &self.name
+    }
+    fn create(self) -> Scope<Self> {
+        let mut cmd = Command::new(cli_bin_path("cloudtruth"));
+        cmd.args(["types", "set", self.name.as_str()]);
+        if let Some(desc) = self.description {
+            cmd.args(["--desc", desc]);
+        }
+        if let Some(parent) = self.parent {
+            cmd.args(["--parent", parent.as_ref()]);
+        }
+        cmd.assert()
+            .success()
+            .stdout(contains!("Created parameter type '{self}'"));
+        Scope::new(self)
+    }
+
+    fn delete(&mut self) {
+        Command::new(cli_bin_path("cloudtruth"))
+            .args(["types", "delete", "--confirm", self.name.as_str()])
+            .assert()
+            .success();
+    }
+}
