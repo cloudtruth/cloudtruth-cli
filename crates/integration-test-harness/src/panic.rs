cd6dbbbdd54466a69d360102d575c940978be5b3
Adam Curtis <kallisti.dev@gmail.com>
2023-04-23 17:28:10 -0400

    improve panic error report structures

--- crates/integration-test-harness/src/panic.rs
+++ crates/integration-test-harness/src/panic.rs
@@ -5,7 +5,7 @@ use std::panic::Location;
 use crate::backtrace;
 use thiserror::Error;
 
-use miette::{Context, Diagnostic, Result};
+use miette::{Diagnostic, Report, Result};
 
 use crate::source_span::TestSourceSpan;
 
@@ -26,13 +26,12 @@ pub fn set_panic_hook_with_caller(caller: &'static Location) {
         } else {
             "Something went wrong".to_string()
         };
-
-        let mut report: Result<()> = Err(PanicReport::new(message).into());
-        if let Some(loc) = info.location() {
-            report = report.with_context(|| {
-                format!("Panic at {}:{}:{}", loc.file(), loc.line(), loc.column())
-            });
-        }
+        let panic = Panic::new(message);
+        let mut report: Result<(), Report> = if let Some(loc) = info.location() {
+            Err(PanicLocation::new(panic, loc).into())
+        } else {
+            Err(panic.into())
+        };
         if let Ok(Some(mut src_span)) = TestSourceSpan::from_backtrace(caller) {
             src_span.add_related(report.unwrap_err());
             report = Err(src_span.into());
@@ -41,27 +40,57 @@ pub fn set_panic_hook_with_caller(caller: &'static Location) {
     }));
 }
 
-#[derive(Debug, Error, Diagnostic)]
+#[derive(Clone, Debug, Error, Diagnostic)]
 #[error("{message}{}", .backtrace.clone().unwrap_or_default())]
 #[diagnostic()]
-pub struct PanicReport {
+pub struct Panic {
     message: String,
-    #[help]
-    help: Option<String>,
     backtrace: Option<String>,
+    #[help]
+    help: Option<&'static str>,
 }
 
-impl PanicReport {
+impl Panic {
     pub fn new(message: String) -> Self {
         let (help, backtrace) = if backtrace::is_rust_backtrace_enabled() {
             (None, Some(backtrace::format_backtrace()))
         } else {
-            (Some(HELP_TEXT.into()), None)
+            (Some(HELP_TEXT), None)
         };
-        PanicReport {
+        Self {
             message,
             help,
             backtrace,
+        }
+    }
+}
+
+#[derive(Debug, Error, Diagnostic)]
+#[error("Panic at {}:{}:{}", filename, line, col)]
+#[diagnostic()]
+pub struct PanicLocation {
+    #[source]
+    #[diagnostic_source]
+    panic: Panic,
+    filename: String,
+    line: u32,
+    col: u32,
+    #[help]
+    help: Option<&'static str>,
+}
+
+impl PanicLocation {
+    fn new(panic: Panic, location: &Location) -> Self {
+        Self {
+            panic,
+            filename: location.file().to_string(),
+            line: location.line(),
+            col: location.column(),
+            help: if backtrace::is_rust_backtrace_enabled() {
+                None
+            } else {
+                Some(HELP_TEXT)
+            },
         }
     }
 }
