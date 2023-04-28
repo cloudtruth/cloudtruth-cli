53b0e1429914f05522c9b0c4b1a33501ced034fe
Adam Curtis <kallisti.dev@gmail.com>
2023-04-28 14:14:58 -0400

    fix typo in test name

--- tests/test_versions.rs
+++ tests/test_versions.rs
@@ -3,20 +3,19 @@ use std::str;
 
 #[test]
 #[use_harness]
-fn test_completions() {
+fn test_versions() {
     let version_cmd = cloudtruth!("--version").assert().success();
     let version = str::from_utf8(&version_cmd.get_output().stdout)
         .unwrap()
         .split(' ')
         .last()
         .unwrap()
-        .trim()
-        .to_string();
+        .trim();
 
     cloudtruth!("version check")
         .assert()
         .success()
-        .stdout(contains(&version));
+        .stdout(contains(version));
 
     cloudtruth!("version install -f").assert();
 
