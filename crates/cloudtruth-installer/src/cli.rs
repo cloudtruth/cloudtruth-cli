536da69f73ba802845bf10491ad063824754a710
Adam Curtis <kallisti.dev@gmail.com>
2023-05-04 18:54:07 -0400

    add CI detection

--- crates/cloudtruth-installer/src/cli.rs
+++ crates/cloudtruth-installer/src/cli.rs
@@ -40,9 +40,7 @@ fn init_globals(cli: &Cli) {
     GLOBALS
         .set(Globals {
             verbose: cli.verbose,
-            interactive: !cli.non_interactive
-                && (cli.interactive
-                    || (std::io::stdin().is_terminal() && std::io::stdout().is_terminal())),
+            interactive: cli.is_interactive(),
         })
         .expect("CLI globals were initialized twice")
 }
@@ -63,6 +61,14 @@ pub struct Cli {
     /// Force non-interactive mode, do not prompt or ask for confirmations
     #[arg(global = true, short = 'n', long, overrides_with = "interactive")]
     non_interactive: bool,
+}
+
+impl Cli {
+    pub fn is_interactive(&self) -> bool {
+        !self.non_interactive
+            && (self.interactive
+                || !is_ci() && std::io::stdin().is_terminal() && std::io::stdout().is_terminal())
+    }
 }
 
 #[derive(Debug, clap::Subcommand)]
@@ -101,4 +107,41 @@ pub struct GitHubOptions {
     )]
     /// GitHub API Release ID
     release_id: Option<String>,
+}
+
+/// Helper to detect common CI environment variables
+fn is_ci() -> bool {
+    macro_rules! check_ci_vars {
+        ($name:literal $(,$names:literal)*$(,)?) => { std::env::var_os($name).is_some() $(|| std::env::var_os($names).is_some())* }
+    }
+    /// List is from watson/ci-info
+    static IS_CI: OnceCell<bool> = OnceCell::new();
+    *IS_CI.get_or_init(|| {
+        check_ci_vars!(
+            "CI",
+            "BUILD_ID",
+            "BUILD_NUMBER",
+            "CI_APP_ID",
+            "CI_BUILD_ID",
+            "CI_BUILD_NUMBER",
+            "CI_NAME",
+            "CONTINUOUS_INTEGRATION",
+            "RUN_ID",
+            "CIRCLECI",
+            "GITLAB_CI",
+            "APPVEYOR",
+            "DRONE",
+            "MAGNUM",
+            "SEMAPHORE",
+            "JENKINS_URL",
+            "bamboo_planKey",
+            "TF_BUILD",
+            "TEAMCITY_VERSION",
+            "BUILDKITE",
+            "HUDSON_URL",
+            "GO_PIPELINE_LABEL",
+            "BITBUCKET_COMMIT",
+            "GITHUB_ACTIONS",
+        )
+    })
 }
