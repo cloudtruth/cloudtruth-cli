23c44d7b1d08a53ffaa74d26c1ce2aa9680fc072
Adam Curtis <kallisti.dev@gmail.com>
2023-06-06 19:09:52 -0400

    try to find arm7 ubuntu package

--- xtask/scripts/install-build-packages-linux.sh
+++ xtask/scripts/install-build-packages-linux.sh
@@ -7,7 +7,7 @@ case $TARGET in
     ;;
     armv7-unknown-linux-gnueabihf)
         sudo apt-get -y update
-        sudo apt-get -y install gcc-7-arm-linux-gnueabihf
+        sudo apt-get -y install gcc-arm-linux-gnueabihf
     ;;
     aarch64-unknown-linux-gnu)
         sudo apt-get -y update
