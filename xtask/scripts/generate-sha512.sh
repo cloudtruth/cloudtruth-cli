fe30dbcd92bcae38d008bbc892126681bc494edb
Adam Curtis <kallisti.dev@gmail.com>
2023-06-16 14:17:07 -0400

    fix case when generating checksum on macOS

--- xtask/scripts/generate-sha512.sh
+++ xtask/scripts/generate-sha512.sh
@@ -4,5 +4,13 @@ INPUT_FILE="$1"
 OUTPUT_VAR="$2"
 
 CHECKSUM_FILE="$(basename "${INPUT_FILE}").sha512"
-sha512sum "${INPUT_FILE}" > "${CHECKSUM_FILE}"
+case $(uname -a) in 
+    *Darwin*)
+        shasum -a 512 "${INPUT_FILE}" > "${CHECKSUM_FILE}"
+    ;;
+    *)
+        sha512sum "${INPUT_FILE}" > "${CHECKSUM_FILE}"
+    ;;
+esac
+
 echo "${OUTPUT_VAR}=${CHECKSUM_FILE}" >> "$GITHUB_ENV"
