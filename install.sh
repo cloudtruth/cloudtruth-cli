cfe389aaa9b7a8b3d606cb5693059a1928f7a3da
Adam Curtis <adam.curtis@cloudtruth.com>
2023-06-21 16:51:12 -0400

    upgrade install script to use wget or curl (#513)
    * upgrade install script to use wget or curl, avoid installing any prerequisites
    
    * use local variables where appropriate
    
    * normalize flag variable logic throughout install script
    
    * improve comments and function names a bit
    
    * make no prereq the default and add --install-prerequisites option
    
    * bug fixes
    
    * add trailing newline to dockerfile template
    
    * check for debian issue before short-circuiting return
    
    * cleanup order of require_cmd calls to allow for prereq install
    
    * define dry_run_opt when not in use
    
    * add --location-trusted option to curl when installing with auth token
    
    * fix typo
    
    * add debug flag to test releases
    
    * re-enable set-e in install script to exit early on command failure
    
    * only install curl if wget not present
    
    * clean up error handling
    
    * change rust-cache shared-key to key
    
    * move busybox detection logic to function and remove --verbose flag
    
    * add docker build arg for --install-prerequisites flag of install script
    
    * Revert "change rust-cache shared-key to key"
    
    This reverts commit 451bd031dde7699dff02c95eb055462d199c34b2.
    
    * turn debugging flag
    
    * dont capture output of download command and pipe directly instead; add --binary option to download command for application/octet-stream
    
    * remove unnecessary checks in install.sh script and dockerfile template
    
    * simplify handling of dry-run install options
    
    * remove broken docker detection logic for deb (Dockerfile itself handles this case)

--- install.sh
+++ install.sh
@@ -1,175 +1,261 @@
 #!/usr/bin/env sh
+# shellcheck shell=dash
 #
-# Copyright (C) 2021 CloudTruth, Inc.
+# Copyright (C) 2023 CloudTruth, Inc.
 #
 
-set -e
-
-### Control     ############################################################
+set -u
 
 CT_CLI_VERSION=
 CT_DRAFT_AUTH_TOKEN=
 CT_DRAFT_RELEASE_ID=
-CT_DRY_RUN=0
-CT_INSTALL_PREREQUISITES=1
-CURL_OPTS="-fsSL"
+CT_DRY_RUN=
+CT_DEBUG=
+CT_INSTALL_PREREQUISITES=
 
-### Detection     ############################################################
+### help text
+usage() {
+    cat <<EOF
+Usage: install.sh [ OPTIONS ]
 
-# detect the architecture
-ARCH=$(uname -m)
+OPTIONS:
 
-if [ -z "${ARCH}" ]; then
-    >&2 echo "Cannot determine system architecture."
-    exit 42
-fi
+  -d | --debug                  enable shell debug output
+  -h | --help                   show usage
+  -i | --install-prerequisites  do not attempt to install prerequisites"
+  -v | --version <VER>          use a specific version
+  -y | --dry-run                download but do not install
 
-# detect which package type the system wants
-OS=$(uname)
-PKG=
-if [ "${OS}" = "Linux" ]; then
-    command -v apk && PKG=apk
-    command -v apt-get && PKG=deb
-    command -v yum && PKG=rpm
+These options are only used for testing during the CloudTruth release workflow:
 
-elif [ "${OS}" = "Darwin" ]; then
-    PKG=macos
-fi
+  -a | --auth-token <TOK>  authorization token to access draft release
+  -r | --release-id <ID>   identity of draft release
+EOF
+}
 
-if [ -z "${PKG}" ]; then
-    >&2 echo "[error] cannot determine system package format"
-    exit 43
-fi
+### entry point for installer script
+main() {
+    parse_opts "$@"
+    check_privs
+    require_cmd uname
+    require_cmd mktemp
+    require_cmd rm
+    require_cmd tar
+    get_target_info
+    if [ -n "${CT_INSTALL_PREREQUISITES}" ]; then
+        install_prerequisites
+    fi
+    require_download_cmd
+    # only required for draft release
+    if [ -n "${CT_DRAFT_RELEASE_ID}" ]; then
+        require_cmd jq
+    fi
+    # set up cleanup handler
+    ORIG_DIR=$(pwd)
+    TMP_DIR=$(mktemp -d)
+    trap cleanup EXIT
+    cd "${TMP_DIR}" || fail "Could not enter temp directory: ${TMP_DIR}"
+    get_cloudtruth_version
+    install_cloudtruth
+}
 
-### Arguments     ############################################################
-while true; do
-    case $1 in
-      (-a|--auth-token)
+### Detect command-line options
+parse_opts() {
+    while true; do
+        case "${1:-}" in
+        (-a|--auth-token)
             CT_DRAFT_AUTH_TOKEN=$2
             shift 2;;
-      (-d|--debug)
+        (-d|--debug)
             echo "[debug] enabled"
-            CURL_OPTS="${CURL_OPTS}v"
+            CT_DEBUG=1
             set -x
             shift;;
-      (-y|--dry-run)
+        (-y|--dry-run)
             CT_DRY_RUN=1
             echo "[dry-run] enabled"
             shift;;
-      (-h|--help)
-            echo "Usage: install.sh [ OPTIONS ]"
-            echo ""
-            echo "OPTIONS:"
-            echo ""
-            echo "  -d | --debug             enable shell debug output"
-            echo "  -h | --help              show usage"
-            echo "  -n | --no-prerequisites  do not attempt to install prerequisites"
-            echo "  -v | --version <VER>     use a specific version"
-            echo "  -y | --dry-run           download but do not install (may fail if prerequisites are missing)"
-            echo ""
-            echo "These options are only used for testing during the CloudTruth release workflow:"
-            echo ""
-            echo "  -a | --auth-token <TOK>  authorization token to access draft release"
-            echo "  -r | --release-id <ID>   identity of draft release"
-            echo ""
-            exit 2;;
-      (-n|--no-prerequisites)
-            CT_INSTALL_PREREQUISITES=0
+        (-h|--help)
+            usage
+            exit 1;;
+        (-i|--install-prerequisites)
+            CT_INSTALL_PREREQUISITES=1
             shift;;
-      (-r|--release-id)
+        (-r|--release-id)
             CT_DRAFT_RELEASE_ID=$2
             shift 2;;
-      (-v|--version)
+        (-v|--version)
             CT_CLI_VERSION=$2
             shift 2;;
-      (--)  shift; break;;
-      (*)
-            if [ -n "${1}" ]; then
+        (--)  shift; break;;
+        (*)
+            if [ -n "${1:-}" ]; then
                 echo "[error] invalid parameter: ${1}"
                 exit 1; # error
             fi
             break;;
-    esac
-done
-# remaining="$@"
+        esac
+    done
+}
 
-### Prerequisites ############################################################
+### detect target platform information
+get_target_info() {
+    # detect the architecture
+    ARCH=$(uname -m)
+    if [ -z "${ARCH}" ]; then
+        fail "Cannot determine system architecture."
+    fi
 
-PREREQUISITES="curl"
-if [ -n "${CT_DRAFT_RELEASE_ID}" ]; then
-  # additional requirements to handle GitHub draft release integration testing
-  PREREQUISITES="${PREREQUISITES} ca-certificates jq"
-fi
+    # detect which package type the system wants
+    OS=$(uname)
+    PKG=
+    if [ "${OS}" = "Linux" ]; then
+        check_cmd apk && PKG=apk
+        check_cmd apt-get && PKG=deb
+        check_cmd yum && PKG=rpm
+    elif [ "${OS}" = "Darwin" ]; then
+        PKG=macos
+    else
+        fail "Unsupported operating system: ${OS}"
+    fi
+
+    if [ -z "${PKG}" ]; then
+        fail "Cannot determine system package format"
+    fi
+}
 
 # shellcheck disable=SC2086
-prerequisites() {
+install_prerequisites() {
+    local prereqs
+    # determine prereqs
+    prereqs=
+    # check for curl or wget; prefer curl
+    if ! check_download_cmd; then
+        prereqs="${prereqs} curl"
+    fi
+    # additional requirements to handle GitHub draft release integration testing
+    if [ -n "${CT_DRAFT_RELEASE_ID}" ]; then
+        prereqs="${prereqs} ca-certificates"
+        # jq is needed for draft release
+        if ! check_cmd jq; then
+            # jq is in centos7 epel repository
+            if [ "${PKG}" = "rpm" ] && [ "$(rpm -E "%{rhel}")" -eq 7 ]; then
+              prereqs="${prereqs} epel-release"
+            else
+                prereqs="${prereqs} jq"
+            fi
+        fi
+    # elif [ "${PKG}" = "deb" ]; then
+    #     # had problems downloading from GitHub on debian buster without ca-certificates update
+    #     prereqs="${prereqs} ca-certificates"
+    fi
+    # skip if all prereqs installed
+    if [ -z "${prereqs}" ]; then
+        return 0
+    fi
+    # install prereqs
     case "$PKG" in
         (apk)
             # alpine - no package format yet, use generic
-            if [ ${CT_DRY_RUN} -ne 0 ]; then
-                CT_PREREQ_DRY_RUN="--simulate"
-            fi
-            apk add ${CT_PREREQ_DRY_RUN} ${PREREQUISITES}
+            apk add ${CT_DRY_RUN:+ --simulate} ${prereqs}
             ;;
         (deb)
             # debian based
-            # had problems downloading from GitHub on debian buster without ca-certificates update
-            PREREQUISITES="${PREREQUISITES} ca-certificates"
-            if [ ${CT_DRY_RUN} -ne 0 ]; then
-                CT_PREREQ_DRY_RUN="--dry-run"
-            fi
-            if [ -f /.dockerenv ]; then
-                apt-get update
-            fi
-            apt-get install --no-install-recommends --yes ${CT_PREREQ_DRY_RUN} ${PREREQUISITES}
-            if [ -f /.dockerenv ]; then
-                apt-get purge
-            fi
+            apt-get install --no-install-recommends --yes ${CT_DRY_RUN:+ --dry-run} ${prereqs}
             ;;
         (rpm)
             # rockylinux, centos, rhel
-            if [ ${CT_DRY_RUN} -ne 0 ]; then
-                CT_PREREQ_DRY_RUN="--setopt tsflags=test"
-            fi
-            if [ -n "${CT_DRAFT_RELEASE_ID}" ] && [ "$(rpm -E "%{rhel}")" -eq 7 ]; then
-              # jq is needed for draft release parsing, jq is in centos7 epel repository
-              yum -y install ${CT_PREREQ_DRY_RUN} epel-release
-              yum repolist
-            fi
-            yum -y install ${CT_PREREQ_DRY_RUN} ${PREREQUISITES}
+            yum -y install ${CT_DRY_RUN:+ --setopt tsflags=test} ${prereqs}
             ;;
     esac
 }
 
-if [ ${CT_INSTALL_PREREQUISITES} -eq 1 ]; then
-  prerequisites
-fi
-
-### Auto-Version  ############################################################
-
-if [ -z "${CT_CLI_VERSION}" ]; then
-    CT_VER_FILE_URL="https://api.github.com/repos/cloudtruth/cloudtruth-cli/releases/latest"
-    CT_CLI_VERSION=$(curl "${CURL_OPTS}" "${CT_VER_FILE_URL}" | \
-              grep "tag_name" | \
-              sed -E 's/.*"([^"]+)".*/\1/')
-    echo "[cloudtruth] found latest version: ${CT_CLI_VERSION}"
-else
-    echo "[cloudtruth] using requested version: ${CT_CLI_VERSION}"
-fi
-
-### Install       ############################################################
-
-cleanup() {
-  cd "${ORIG_DIR}"
-  rm -r "${TMP_DIR}"
+### Detect which version to install
+get_cloudtruth_version() {
+    local latest_version_url
+    if [ -z "${CT_CLI_VERSION}" ]; then
+        latest_version_url="https://api.github.com/repos/cloudtruth/cloudtruth-cli/releases/latest"
+        CT_CLI_VERSION=$(download "${latest_version_url}" | \
+            grep "tag_name" | \
+            sed -E 's/.*"([^"]+)".*/\1/')
+        echo "[cloudtruth] found latest version: ${CT_CLI_VERSION}"
+    else
+        echo "[cloudtruth] using requested version: ${CT_CLI_VERSION}"
+    fi
 }
 
-ORIG_DIR=$(pwd)
-TMP_DIR=$(mktemp -d)
-trap cleanup EXIT
-cd "${TMP_DIR}"
+### Download and install cloudtruth CLI
+install_cloudtruth() {
+    local target_name
+    local package_dir
+    local package
+    # alpine, macos - no package format yet, use generic binary
+    if [ "${PKG}" = "apk" ] || [ "${PKG}" = "macos" ]; then
+        # normalize CPU arch
+        case $ARCH in
+            arm64 | armv8l | armv8b)
+                ARCH="aarch64"
+            ;;
+            armv7l)
+                ARCH="armv7"
+            ;;
+            armv6l)
+                ARCH="arm"
+            ;;
+        esac
+        # determine taret name from OS (default to linux)
+        if [ "${OS}" = "Darwin" ]; then
+            target_name=apple-darwin
+        elif [ "${ARCH}" = "aarch64" ]; then
+            target_name=unknown-linux-musl
+        elif [ "${ARCH}" = "arm" ] || [ "${ARCH}" = "armv7" ]; then
+            target_name=unknown-linux-musleabihf
+        else
+            target_name=unknown-linux-musl
+        fi
+        package_dir="cloudtruth-${CT_CLI_VERSION}-${ARCH}-${target_name}"
+        package="${package_dir}.tar.gz"
+        download_asset "${package}" || fail "Couldn't download release package: ${package}"
+        tar xzf "${package}" || fail "Couldn't unpack release archive: ${package}"
+        if [ -n "${CT_DRY_RUN}" ]; then
+            echo "[dry-run] skipping install of ${package_dir}/cloudtruth"
+        else
+            install -m 755 -o root "${package_dir}/cloudtruth" /usr/local/bin
+        fi
+    fi
 
-download() {
+    # debian based
+    if [ "${PKG}" = "deb" ]; then
+        if [ "${ARCH}" = "x86_64" ]; then
+            ARCH="amd64"
+        fi
+        package=cloudtruth_${CT_CLI_VERSION}_${ARCH}.deb
+        download_asset "${package}"|| fail "Couldn't download release package: ${package}"
+        if [ -n "${CT_DRY_RUN}" ]; then
+            echo "[dry-run] skipping install of ${package}"
+        else
+            dpkg -i "${package}"
+        fi
+    fi
+
+    # rpm based
+    if [ "${PKG}" = "rpm" ]; then
+        package=cloudtruth-${CT_CLI_VERSION}-1.${ARCH}.rpm
+        download_asset "${package}"|| fail "Couldn't download release package: ${package}"
+        if [ -n "${CT_DRY_RUN}" ]; then
+            echo "[dry-run] skipping install of ${package}"
+        else
+            rpm -i "${package}"
+        fi
+    fi
+
+    if [ -z "${CT_DRY_RUN}" ]; then
+        echo "[cloudtruth] installed: $(cloudtruth --version)"
+    fi
+}
+
+### Download CloudTruth CLI
+download_asset() {
     if [ -z "${CT_DRAFT_RELEASE_ID}" ]; then
       download_release "$1"
     else
@@ -177,91 +263,136 @@ download() {
     fi
 }
 
-# this is used to download release assets
+### This used to download release assets
 download_release() {
+    local package
+    local base_url
+    local download_url
     package=$1
     base_url="https://github.com/cloudtruth/cloudtruth-cli/releases/download"
     download_url="${base_url}/${CT_CLI_VERSION}/${package}"
-    curl "${CURL_OPTS}" -H "Accept: application/octet-stream" -o "${package}" "${download_url}"
+    download --binary "${download_url}" "${package}"
 }
 
-# this is used to download a draft release during integration testing
+### This is used to download a draft release during integration testing
 download_draft() {
+    local package
+    local assetfile
+    local asset_id
+    local download_url
     package=$1
     assetfile="${CT_DRAFT_RELEASE_ID}.assets.json"
 
     # get all the assets for the release
-    curl "${CURL_OPTS}" -H "Authorization: token ${CT_DRAFT_AUTH_TOKEN}" -o "${assetfile}" \
-        "https://api.github.com/repos/cloudtruth/cloudtruth-cli/releases/${CT_DRAFT_RELEASE_ID}/assets"
+    download \
+        "https://api.github.com/repos/cloudtruth/cloudtruth-cli/releases/${CT_DRAFT_RELEASE_ID}/assets" \
+        "${assetfile}"
 
     # find the asset id for the given package
     asset_id=$(jq ".[] | select(.name==\"${package}\") | .id" "${assetfile}")
     rm "${assetfile}"
 
     download_url="https://api.github.com/repos/cloudtruth/cloudtruth-cli/releases/assets/${asset_id}"
-    curl "${CURL_OPTS}" --location-trusted -H "Authorization: token ${CT_DRAFT_AUTH_TOKEN}" -H "Accept: application/octet-stream" -o "${package}" "${download_url}"
+    download --binary "${download_url}" "${package}"
 }
 
-# alpine, macos - no package format yet, use generic binary
-if [ "${PKG}" = "apk" ] || [ "${PKG}" = "macos" ]; then
-    # normalize CPU arch
-    case $ARCH in
-        arm64 | armv8l | armv8b)
-            ARCH="aarch64"
-        ;;
-        armv7l)
-            ARCH="armv7"
-        ;;
-        armv6l)
-            ARCH="arm"
-        ;;
-    esac
-    # determine taret name from OS (default to linux)
-    if [ "${OS}" = "Darwin" ]; then
-        TARGET_NAME=apple-darwin
-    elif [ "${ARCH}" = "aarch64" ]; then
-        TARGET_NAME=unknown-linux-musl
-    elif [ "${ARCH}" = "arm" ] || [ "${ARCH}" = "armv7" ]; then
-        TARGET_NAME=unknown-linux-musleabihf
+### Uses either curl or wget to download something, depending on what is available
+download() {
+    local dl_cmd
+    local status
+    local accept_header
+    if check_cmd curl; then
+        dl_cmd=curl
+    elif check_cmd wget; then
+        dl_cmd=wget
     else
-        TARGET_NAME=unknown-linux-musl
+        require_download_cmd # no downloader; show error message and exit
     fi
-    PACKAGE_DIR="cloudtruth-${CT_CLI_VERSION}-${ARCH}-${TARGET_NAME}"
-    PACKAGE="${PACKAGE_DIR}.tar.gz"
-    download "${PACKAGE}"
-    tar xzf "${PACKAGE}"
-    if [ ${CT_DRY_RUN} -ne 0 ]; then
-        echo "[dry-run] skipping install of ${PACKAGE_DIR}/cloudtruth"
+    # check for --binary option
+    if [ "$1" = '--binary' ]; then
+        accept_header='application/octet-stream'
+        shift
+    fi
+    if [ "$dl_cmd" = curl ]; then
+        curl \
+            --retry 3 --proto '=https' --tlsv1.2 --silent --show-error --fail --location \
+            ${CT_DRAFT_AUTH_TOKEN:+ --location-trusted -H "Authorization: token ${CT_DRAFT_AUTH_TOKEN}"} \
+            ${CT_DEBUG:+ --verbose} \
+            ${accept_header:+ -H "Accept: ${accept_header}"} \
+            "$1" \
+            ${2:+ --output "$2"}
+        status=$?
+    elif [ "$dl_cmd" = wget ]; then
+        if check_busybox_wget; then
+            echo "Warning: using the BusyBox version of wget.  Not enforcing strong cipher suites for TLS or TLS v1.2, this is potentially less secure"
+            wget \
+                ${CT_DRAFT_AUTH_TOKEN:+ --header="Authorization: token ${CT_DRAFT_AUTH_TOKEN}"} \
+                ${accept_header:+ --header="Accept: ${accept_header}"} \
+                "$1" \
+                ${2:+ -O "$2" }
+            status=$?
+        else
+            wget \
+                --https-only --secure-protocol=TLSv1_2 \
+                ${CT_DRAFT_AUTH_TOKEN:+ --header="Authorization: token ${CT_DRAFT_AUTH_TOKEN}"} \
+                ${accept_header:+ --header="Accept: ${accept_header}"} \
+                ${CT_DEBUG:+ --verbose} \
+                "$1" \
+                ${2:+ -O "$2" }
+            status=$?
+        fi
     else
-        install -m 755 -o root "${PACKAGE_DIR}/cloudtruth" /usr/local/bin
+        fail "Could not find download command '$dl_cmd'"
     fi
-fi
+    return $status
+}
 
-# debian based
-if [ "${PKG}" = "deb" ]; then
-    if [ "${ARCH}" = "x86_64" ]; then
-        ARCH="amd64"
-    fi
-    PACKAGE=cloudtruth_${CT_CLI_VERSION}_${ARCH}.deb
-    download "${PACKAGE}"
-    if [ ${CT_DRY_RUN} -ne 0 ]; then
-        echo "[dry-run] skipping install of ${PACKAGE}"
-    else
-        dpkg -i "${PACKAGE}"
-    fi
-fi
+### Exit with a helpful message
+fail() {
+    echo "[error] $1" >&2
+    exit 1
+}
 
-# rpm based
-if [ "${PKG}" = "rpm" ]; then
-    PACKAGE=cloudtruth-${CT_CLI_VERSION}-1.${ARCH}.rpm
-    download "${PACKAGE}"
-    if [ ${CT_DRY_RUN} -ne 0 ]; then
-        echo "[dry-run] skipping install of ${PACKAGE}"
-    else
-        rpm -i "${PACKAGE}"
+### Checks for a command on the system and exits with a helpful message if not found
+require_cmd() {
+    if ! check_cmd "$1"; then
+        fail "This install script requires the '$1' command, but it was not found."
     fi
-fi
+}
 
-if [ ${CT_DRY_RUN} -eq 0 ]; then
-    echo "[cloudtruth] installed: $(cloudtruth --version)"
-fi
+### Checks for a command on the system
+check_cmd() {
+    command -v "$1" > /dev/null 2>&1
+}
+
+### Check for a download command on the system
+check_download_cmd() {
+    check_cmd curl || check_cmd wget
+}
+
+### Check for a download command on the system and exits with a helpful message if not found
+require_download_cmd() {
+    if ! check_download_cmd; then
+        fail "This install script requires either the curl or wget command, but neither were found."
+    fi
+}
+
+### Installer must run as root
+check_privs() {
+    if [ -z "${CT_DRY_RUN}" ] && [ "$(id -u)" -ne 0 ]; then
+        fail "This install script requires root privileges. Please run with su or sudo."
+    fi
+}
+
+### Detect BusyBox version of wget
+check_busybox_wget() {
+    [ "$(wget -V 2>&1|head -2|tail -1|cut -f1 -d" ")" = "BusyBox" ]
+}
+
+### Clean up on exit
+cleanup() {
+    cd "${ORIG_DIR}" || fail "Could not return to original directory: ${ORIG_DIR}"
+    rm -r "${TMP_DIR}"
+}
+
+main "$@"
