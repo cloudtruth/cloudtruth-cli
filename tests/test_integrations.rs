e265723297dc0a2501d662f239d3c2d831eebad9
Adam Curtis <adam.curtis@cloudtruth.com>
2023-10-20 19:54:01 -0400

    fix issue with UNC path in windows

--- tests/test_integrations.rs
+++ tests/test_integrations.rs
@@ -2,7 +2,7 @@ use std::{env, error::Error, path::PathBuf, process::Command};
 
 #[test]
 fn test_integrations_pytest() -> Result<(), Box<dyn Error>> {
-    let cargo_manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+    let cargo_manifest_dir = dunce::canonicalize(PathBuf::from(env::var("CARGO_MANIFEST_DIR")?))?;
     let live_test = cargo_manifest_dir.join("pytest/live_test.py");
     let mut handle = Command::new("python3")
         .current_dir(cargo_manifest_dir)
